43dd71b6a3b4c65d211812c68e506bf8547fc2bb
kaos1180 <45263972+kaos1180@users.noreply.github.com>
2023-11-27 16:52:39 +0530

    fixed comments and added functions for pomodoro

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Get references to HTML elements
     const taskList = document.getElementById('taskList');
     const addTaskForm = document.getElementById('addTaskForm');
+    const startPomodoroButton = document.getElementById('startPomodoro');
 
+    // Event listener for the task addition form
     addTaskForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
+        // Get task details from form
         const taskTitle = document.getElementById('taskTitle').value;
         const taskDate = document.getElementById('taskDate').value;
         const taskTime = document.getElementById('taskTime').value;
 
+        // Check if the task title is not empty
         if (taskTitle.trim() !== '') {
             // Create a new task element
             const taskElement = document.createElement('div');
             taskElement.classList.add('task');
-            
-            // Parse the date and time input values into a Date object
+
+            // Parse due date and time
             const dueDateTime = new Date(`${taskDate}T${taskTime}`);
-            
+
+            // Populate task element with details
             taskElement.innerHTML = `
                 <span>${taskTitle}</span>
                 <span class="due-date">${dueDateTime.toLocaleDateString()}</span>
@@ -24,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <button onclick="deleteTask(this)">Delete</button>
             `;
 
-            // Add the task to the task list
+            // Add the task element to the task list
             taskList.appendChild(taskElement);
 
             // Clear the input fields
@@ -33,10 +39,28 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('taskTime').value = '';
         }
     });
+
+    // Event listener for the Pomodoro button
+    startPomodoroButton.addEventListener('click', startPomodoro);
 });
 
+// Function to delete a task
 function deleteTask(button) {
-    // Remove the task when the delete button is clicked
     const taskElement = button.closest('.task');
     taskElement.remove();
+}
+
+// Function to start the Pomodoro timer
+function startPomodoro() {
+    // Get the Pomodoro duration from the input field
+    const pomodoroDuration = document.getElementById('pomodoroDuration').value;
+
+    // Check if the input is a valid number greater than 0
+    if (!isNaN(pomodoroDuration) && pomodoroDuration > 0) {
+        // Display an alert (replace with your Pomodoro timer logic)
+        alert(`Pomodoro started! Work session duration: ${pomodoroDuration} minutes`);
+    } else {
+        // Display an alert for invalid input
+        alert('Please enter a valid Pomodoro duration.');
+    }
 }
